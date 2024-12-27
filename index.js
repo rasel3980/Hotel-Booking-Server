358566060ba7d83ed83a86d29a2770487eddab9f
rasel3980 <rkrasel3980@gmail.com>
2024-12-27 17:33:18 +0600

    Initial json data stored

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n4ll4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,13 +30,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const roomsCollection = client.db("hotelDB").collection("rooms");
+  
+
+
+
+    app.get('/room-data', async( req, res)=>{
+      const result = await roomsCollection.find().toArray()
+      console.log(result);
+      res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
