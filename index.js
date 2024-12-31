8c1ab768d1e6d20a886aba6c6271c5fd67d0506b
rasel3980 <rkrasel3980@gmail.com>
2024-12-31 18:05:29 +0600

    updated success

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,10 +13,7 @@ app.use(express.json());
 // pass = rCYIGDmYsRbSh5B8
 // user = hotelBooking
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n4ll4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,7 +21,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -33,38 +30,44 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
     const roomsCollection = client.db("hotelDB").collection("rooms");
     const RoomBookedCollection = client.db("hotelDB").collection("booked");
-  
 
-
-// get all rooms
-    app.get('/room-data', async( req, res)=>{
-      const result = await roomsCollection.find().toArray()
+    // get all rooms
+    app.get("/room-data", async (req, res) => {
+      const result = await roomsCollection.find().toArray();
       // console.log(result);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // rooms details
-      app.get("/rooms/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await roomsCollection.findOne(query);
-        res.send(result);
-      });
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+    });
 
-        app.post('/my-booked-room', async(req,res)=>{
-          const MyRoom = req.body;
-          const result = await RoomBookedCollection.insertOne(MyRoom);
-          res.send(result);
-        })
-      //  getting data by email
+    app.post("/my-booked-room", async (req, res) => {
+      const MyRoom = req.body;
+      const result = await RoomBookedCollection.insertOne(MyRoom);
+      res.send(result);
+    });
+    //  getting data by email
     app.get("/my-booked-room", async (req, res) => {
       const email = req.query.email;
       const query = { bookingEmail: email };
       const result = await RoomBookedCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/my-booked-room/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await RoomBookedCollection.findOne(filter);
       res.send(result);
     });
 
@@ -74,43 +77,26 @@ async function run() {
       const result = await RoomBookedCollection.deleteOne(query);
       res.send(result);
     });
-     // update Room
+    // update Room
 
-     app.put("/update-room/:id", async (req, res) => {
+    app.put("/update-room/:id", async (req, res) => {
       const id = req.params.id;
-      const { 
-        price_per_night, 
-        room_name, 
-        room_type, 
-        photo, 
-        availability_status, 
-        room_size, 
-        user_reviews, 
-        room_description, 
-        available_amenities, 
-        location 
-    } = req.body;
+      const  {date}  = req.body;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const room = {
         $set: {
-          price_per_night:price_per_night,
-          room_name:room_name,
-          room_type:room_type,
-          photo:photo,
-          availability_status:availability_status,
-          room_size:room_size,
-          user_reviews:user_reviews,
-          room_description:room_description,
-          available_amenities:available_amenities,
-          location:location,
+          date: date,
         },
       };
-      const result = await roomsCollection.updateOne(filter,room,options);
+      const result = await RoomBookedCollection.updateOne(
+        filter,
+        room,
+        options
+      );
+      console.log(result);
       res.send(result);
     });
-
-
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -118,14 +104,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hotel Booking server is Running");
+});
 
-
-
-
-app.get('/', (req,res)=>{
-    res.send('Hotel Booking server is Running');
-})
-
-app.listen(port,()=>{
-    console.log(`hotel booking server is running port:${port}`);
-})
+app.listen(port, () => {
+  console.log(`hotel booking server is running port:${port}`);
+});
